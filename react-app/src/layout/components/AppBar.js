c5e6b79565186d48a635c64ad42a163414f89477
jcorriveau <jonathancorriveau@familiprix.com>
2021-10-22 08:08:29 -0400

    Fix slide of appbar

--- react-app/src/layout/components/AppBar.js
+++ react-app/src/layout/components/AppBar.js
@@ -1,15 +1,16 @@
+import React from 'react';
 import { AppBar as MuiAppBar } from '@mui/material';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import SearchInput from './SearchInput';
 
-const AppBar = () => (
-    <MuiAppBar position="fixed">
+const AppBar = React.forwardRef((props, ref) => (
+    <MuiAppBar {...props} ref={ref}>
       <Toolbar>
         <Box sx={{ flexGrow: 1 }} />
         <SearchInput />
       </Toolbar>
     </MuiAppBar>
-);
+));
 
 export default AppBar;
--- react-app/src/layout/components/LayoutPage.js
+++ react-app/src/layout/components/LayoutPage.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import { Box, Container, Toolbar } from '@mui/material';
+import { Box, Container, Toolbar, Slide } from '@mui/material';
 import AppBar from './AppBar';
-import HideOnScroll from './HideOnScroll';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
 
-export default function LayoutPage({ children }) {
+export default function LayoutPage(props) {
+  function HideOnScroll({ children }) {
+    const trigger = useScrollTrigger();
+
+    return (
+      <Slide appear={false} direction="down" in={!trigger}>
+        {children}
+      </Slide>
+    );
+  }
+
   return (
     <React.Fragment>
-      <HideOnScroll>
+      <HideOnScroll {...props}>
         <AppBar />
       </HideOnScroll>
       <Toolbar /> {/*Hack to position content underneath the AppBar */}
       <Container maxWidth={false}>      
         <Box sx={{ flexGrow: 1 }}>
-          {children}
+          {props.children}
         </Box>
       </Container>
     </React.Fragment>
